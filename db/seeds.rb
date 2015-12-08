c0ae41b4c7fb62ce828d51480448757478f12a2a
Sahil Mehta <samehta91@gmail.com>
2015-12-07 19:25:05 -0800

    Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 
+
 # Create Users
 10.times do
 user = User.new(
@@ -11,7 +12,6 @@ user.skip_confirmation!
 user.save!
 end
 users = User.all
-
 
 #Create Admin User
 admin = User.new(
@@ -32,5 +32,25 @@ admin = User.new(
  standard.save!
 
 
+# Create Posts
+ 50.times do
+   Post.create!(
+     title:  Faker::Lorem.sentence,
+     body:   Faker::Lorem.paragraph
+   )
+ end
+ posts = Post.all
+ 
+ # Create Comments
+ 100.times do
+   Comment.create!(
+     post: posts.sample,
+     body: Faker::Lorem.paragraph
+   )
+ end
+
+
 puts "Seed finished"
 puts "#{User.count} users counted"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
