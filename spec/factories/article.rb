2545c7a53a9960a0ebc67817b478d69ce15a170b
Sahil Mehta <samehta91@gmail.com>
2016-01-08 13:13:56 -0800

    Refactored article feature spec and replaced user id in article factory with association

--- spec/factories/article.rb
+++ spec/factories/article.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :article do
     title "Article Title"
     body "This is the first official Article of FantasyMetrix."
-    user_id 1
+    user
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
-  factory :user do
-  username "Sahil"
-  sequence(:email, 100) { |n| "person#{n}@example.com"}
-  password "password"
-  password_confirmation "password"
-  confirmed_at Time.now
- end
+    factory :user do
+    username "Sahil"
+    sequence(:email, 100) { |n| "person#{n}@example.com"}
+    password "password"
+    password_confirmation "password"
+    confirmed_at Time.now
+  end
 
  factory :admin, class: User do
-  username "Admin"
-  sequence(:email, 100) { |n| "admin#{n}@example.com"}
-  password "password"
-  password_confirmation "password"
-  role "admin"
-  confirmed_at Time.now
- end
+    username "Admin"
+    sequence(:email, 100) { |n| "admin#{n}@example.com"}
+    password "password"
+    password_confirmation "password"
+    role "admin"
+    confirmed_at Time.now
+  end
 end
