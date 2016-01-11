9243b1aad86b7832e13538acbb2b251aca72ac79
Sahil Mehta <samehta91@gmail.com>
2016-01-10 17:20:11 -0800

    Updated user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe User do 
 
   describe "associations" do 
-    it { should have_many(:posts) }  
-    it { should have_many(:comments) } 
+    it { should have_many(:posts).dependent(:destroy) }  
+    it { should have_many(:comments).dependent(:destroy) } 
     it { should have_many(:articles) }   
   end
 end
