a3b291a7dc7ba95bd52a7ee35f23c0215a79d1be
Sahil Mehta <samehta91@gmail.com>
2016-01-12 15:07:27 -0800

    Change data type of migration file for production

--- db/migrate/20160112220643_change_season_type_in_passing_game_logs.rb
+++ db/migrate/20160112220643_change_season_type_in_passing_game_logs.rb
@@ -1,6 +1,6 @@
 class ChangeSeasonTypeInPassingGameLogs < ActiveRecord::Migration
   def self.up
-    change_column :passing_game_logs, :season, :integer
+    change_column :passing_game_logs, :season, 'integer USING CAST("season" AS integer)'
   end
  
   def self.down
