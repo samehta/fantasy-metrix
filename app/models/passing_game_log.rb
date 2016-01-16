ab924722cae91e638de10317d4a64486259086a9
Sahil Mehta <samehta91@gmail.com>
2016-01-16 14:47:25 -0800

    Added completion percentage and yards per attempt methods

--- app/models/passing_game_log.rb
+++ app/models/passing_game_log.rb
@@ -1,3 +1,11 @@
 class PassingGameLog < ActiveRecord::Base
   belongs_to :player
+
+  def completion_percentage
+    ((completions.to_f / passing_attempts.to_f) * 100).round(1)
+  end
+
+  def yards_per_attempt 
+    (passing_yards.to_f / passing_attempts.to_f).round(1)
+  end
 end
