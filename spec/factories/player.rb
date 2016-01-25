3a9da174decd5d7d7973bdabb9213dce6a69615c
Sahil Mehta <samehta91@gmail.com>
2016-01-24 22:40:59 -0800

    Tested method for player's age

---
+++ spec/factories/player.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :player do
+    name "Player"
+    date_of_birth Date.today
+  end
+end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -8,7 +8,12 @@ describe Player do
   end
 
   describe "age" do
-    it "should calculate a player's age from their birth date" do  
+    it "should calculate a player's age from their birth date" do
+      player = create(:player)
+      year_constant = 10000
+      players_age = (Date.today.strftime('%Y%m%d').to_i - player.date_of_birth.strftime('%Y%m%d').to_i) / year_constant
+
+      expect(player.age).to eq(players_age)
     end
   end
 end
