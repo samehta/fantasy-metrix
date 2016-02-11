9b6cc6ebcaa823ac006c093e22693f4a189ff825
Sahil Mehta <samehta91@gmail.com>
2016-02-11 15:10:31 -0800

    Update views for ranking and statistics

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   validates_uniqueness_of :username, case_sensitive: false
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 
   def admin?
     role == 'admin'
