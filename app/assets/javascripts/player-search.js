e76460eab44a07545bad9caa222f340458f001db
Sahil Mehta <samehta91@gmail.com>
2016-02-11 10:15:12 -0800

    Updated player search to render player page

--- app/assets/javascripts/player-search.js
+++ app/assets/javascripts/player-search.js
@@ -2,3 +2,8 @@ $(document).ready(function() {
   var playerNames = $('input[data-player-search]').data('player-search');
   $('input[data-player-search]').typeahead({ source: playerNames });
 });
+
+function getPlayer() {
+  document.location.href = '/players/josh-mccown';
+  return false;
+}
