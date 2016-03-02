aa597301b64a4355ca8a7c217c40daf79bf9cf2d
Sahil Mehta <samehta91@gmail.com>
2016-03-02 09:28:38 -0800

    Changed default order of player images on teams page

--- app/models/nfl_team.rb
+++ app/models/nfl_team.rb
@@ -2,7 +2,7 @@ class NflTeam < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
   
-  has_many :nfl_players
+  has_many :nfl_players, -> { order('created_at ASC') }
 
   default_scope { order('created_at ASC') }
 end
